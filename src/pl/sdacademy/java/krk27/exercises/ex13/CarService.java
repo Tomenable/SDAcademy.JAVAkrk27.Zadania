107c8704d55331ab4908e7847ce1bc486c5d7001
Jakub Fik <fikkuba@gmail.com>
2021-01-23 16:00:38 +0100

    Zadanie 13

--- src/pl/sdacademy/java/krk27/exercises/ex13/CarService.java
+++ src/pl/sdacademy/java/krk27/exercises/ex13/CarService.java
@@ -4,7 +4,9 @@ import pl.sdacademy.java.krk27.exercises.ex12.Car;
 import pl.sdacademy.java.krk27.exercises.ex12.Engine;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CarService {
@@ -27,5 +29,22 @@ public class CarService {
     public List<Car> getCarV12(){
         return listaSamochodow.stream().filter( s -> s.getTypSilnika().equals(Engine.V12)).collect(Collectors.toList());
     }
-
+    public List<Car> before1999()
+    {
+        return listaSamochodow.stream()
+                .filter(car -> car.getRokProdukcji() < 1999)
+                .collect(Collectors.toList());
+    }
+    public Car getMostExpensiveCar(){
+        Optional<Car> car = listaSamochodow.stream()
+                .sorted(Comparator.comparing(Car::getCena, Comparator.reverseOrder()))
+                .findFirst();
+        return car.isPresent()? car.get() : null;
+    }
+    public Car getCheapestCar() {
+        Optional<Car> car = listaSamochodow.stream()
+                .sorted(Comparator.comparing(Car::getCena))
+                .findFirst();
+        return car.isPresent() ? car.get() : null;
+    }
 }
