564963317f0f754c56e392d15ee1675980770f70
Paweł Łęczycki <pawel@msd.info.pl>
2021-01-23 12:01:39 +0100

    Zadanie 3 - rozbudowa

--- src/pl/sdacademy/java/krk27/exercises/ex3/Main.java
+++ src/pl/sdacademy/java/krk27/exercises/ex3/Main.java
@@ -1,5 +1,6 @@
 package pl.sdacademy.java.krk27.exercises.ex3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,6 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Map<String,Integer> map = Map.of("Java", 18,"Python", 1,"PHP", 0);
         print(map);
+        print2(map);
+        print3(map);
     }
     public static void print(Map<String,Integer> map) {
         StringBuilder wypisz = new StringBuilder();
@@ -21,5 +24,19 @@ public class Main {
         System.out.println(result);
     }
 
+    public static void print2(Map<String,Integer> map) {
+        List<String> strings = new ArrayList<>();
+        for (var element : map.entrySet()){
+            strings.add("key: " + element.getKey() + ", Value: " + element.getValue());
+        }
+        System.out.println(String.join(",\n", strings) + ".");
+    }
 
+    public static void print3(Map<String,Integer> map) {
+        System.out.println(
+                map.entrySet().stream()
+                .map(element -> "key: " + element.getKey() + ", Value: " + element.getValue())
+                .collect(Collectors.joining(",\n")) + "."
+        );
+    }
 }
