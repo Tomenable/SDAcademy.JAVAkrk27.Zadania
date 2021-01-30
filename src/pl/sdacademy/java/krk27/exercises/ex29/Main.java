1d52d5e470387d9ab88b969bca185f891f7c9fd8
PawelSroka <pawel.h.sroka@gmail.com>
2021-01-30 10:41:24 +0100

    cwiczenie 29

---
+++ src/pl/sdacademy/java/krk27/exercises/ex29/Main.java
@@ -0,0 +1,21 @@
+package pl.sdacademy.java.krk27.exercises.ex29;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] table = IntStream.range(0, 10).boxed().toArray(Integer[]::new);
+        System.out.println(partOf(table, s -> s % 2 == 0) + " %");
+
+    }
+
+    public static <T> double partOf(T[] toAdd, Function<T, Boolean> function) {
+        double iloscSpelniajacaWarunek;
+
+        iloscSpelniajacaWarunek = Arrays.stream(toAdd).filter(s -> function.apply(s)).count();
+        return (iloscSpelniajacaWarunek / toAdd.length) * 100;
+
+    }
+}
